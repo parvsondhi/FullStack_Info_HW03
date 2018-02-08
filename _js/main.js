951d4736dcacded02572ae6b9b3d734d70ad5ee5
Carlo Liquido <carloliquido@Carlos-MacBook-Pro.local>
2018-02-08 12:39:13 -0800

    adding HW03 starter code

---
+++ _js/main.js
@@ -0,0 +1,23 @@
+$(document).ready(
+    $("#new-item").on('click', function() {
+        // once the document loads, create new item with this function
+        var user_input = $('#todo-item-input').val();
+        // alert(user_input);
+
+        $('#list_todo').prepend("<li> <button> Move me! </button>" + user_input + "</li>");
+    })
+);
+
+$("#list_todo").on('click', "button", function() {
+        // move from list_todo container to list_doing container
+        // console.log($(this).parent());
+
+        $(this).html("Add To To-Do");
+
+        var completedItem = $(this).parent()
+        $("#list_doing").prepend(completedItem);
+});
+
+$("#list_doing").on('click', "button", function() {
+        // move back from list_doing container to list_todo container
+});

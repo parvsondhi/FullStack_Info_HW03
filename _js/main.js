0e81973d5c5e60c212bb4a580a0de9a52656fae9
daphnejong <daphne.jong@berkeley.edu>
2018-02-13 16:04:19 -0800

    initial commit with todo and doing

--- _js/main.js
+++ _js/main.js
@@ -4,20 +4,24 @@ $(document).ready(
         var user_input = $('#todo-item-input').val();
         // alert(user_input);
 
-        $('#list_todo').prepend("<li> <button> Move me! </button>" + user_input + "</li>");
+        $('#list_todo').prepend("<li> <button id='listbutton'> Move me! </button>" + user_input + "</li>");
     })
 );
 
-$("#list_todo").on('click', "button", function() {
+$("#list_todo").on('click', "button", "listbutton", function() {
         // move from list_todo container to list_doing container
         // console.log($(this).parent());
-
         $(this).html("Add To To-Do");
 
         var completedItem = $(this).parent()
         $("#list_doing").prepend(completedItem);
 });
 
-$("#list_doing").on('click', "button", function() {
+
+$("#list_doing").on('click', "button", "listbutton", function() {
         // move back from list_doing container to list_todo container
+       $(this).html("Move me!");
+
+        var completedItem = $(this).parent()
+        $("#list_todo").prepend(completedItem);
 });

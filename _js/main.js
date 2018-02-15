fccf9687cb1bbb7edafc22d0f90f74c47092e1ca
Xinhe Ren <jim.x.ren@berkeley.edu>
2018-02-15 03:03:33 -0800

    Finished

--- _js/main.js
+++ _js/main.js
@@ -7,24 +7,28 @@ function genCard(btn, msg) {
     return dom;
 }
 
-$(document).ready(
+$(document).ready(function () {
     $("#new-item").on('click', function() {
         // once the document loads, create new item with this function
         var user_input = $('#todo-item-input').val();
         $('#todo-item-input').val("");
         var newCard = genCard('check', user_input);
         $('#list_todo').prepend(newCard);
-    })
-);
-
-$("#list_todo .card-icon").on('click', function() {
-        // move from list_todo container to list_doing container
-         alert("list todo clicked!");
-
-        var completedItem = $(this).parent()
-        $("#list_doing").prepend(completedItem);
+    });
 });
 
-$("#list_doing a").on('click', function() {
-        // move back from list_doing container to list_todo container
+$("#list_todo").on('click', '.card a', function() {
+    var card = $(this).closest('.card');
+    card.removeClass();
+    card.addClass('card green lighten-4');
+    card.find('.material-icons').text('arrow_back');
+    $("#list_doing").prepend(card);
+});
+
+$("#list_doing").on('click', '.card a', function() {
+    var card = $(this).closest('.card');
+    card.removeClass();
+    card.addClass('card white');
+    card.find('.material-icons').text('check');
+    $("#list_todo").prepend(card);
 });

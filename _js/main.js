bf443deaf9f747235cf6e2cfa13cc6f08b6d3f80
Andy Zhang <y.zhang@berkeley.edu>
2018-02-15 00:21:33 -0800

    move left and right

--- _js/main.js
+++ _js/main.js
@@ -1,23 +1,64 @@
 $(document).ready(
-    $("#new-item").on('click', function() {
+    $("#new-item").on('click', function () {
         // once the document loads, create new item with this function
-        var user_input = $('#todo-item-input').val();
-        // alert(user_input);
-
-        $('#list_todo').prepend("<li> <button> Move me! </button>" + user_input + "</li>");
+        var user_input = $('#new-item-input').val();
+        if (user_input == "") {
+            return;
+        }
+        
+        var new_entry = $("<li>").html(user_input).append("<button class='move-right'> Move right </button>").prepend("<button class='move-left'> Move left </button>");
+        
+        new_entry.find(".move-left").hide();
+        
+        $('#list-todo').prepend(new_entry);
     })
 );
 
-$("#list_todo").on('click', "button", function() {
-        // move from list_todo container to list_doing container
-        // console.log($(this).parent());
-
-        $(this).html("Add To To-Do");
-
-        var completedItem = $(this).parent()
-        $("#list_doing").prepend(completedItem);
+$(".column").on('click', '.move-right', function () {
+    var completed_item = $(this).parent();
+    completed_item.find(".move-left").show();
+    
+    var next_column = getNextColumn(completed_item);
+    
+    if (isLastColumn(next_column)) {
+        completed_item.find(".move-right").hide();
+    }
+    
+    getNextColumn(completed_item).find(".column-entries").prepend(completed_item);
 });
 
-$("#list_doing").on('click', "button", function() {
-        // move back from list_doing container to list_todo container
+$(".column").on('click', '.move-left', function () {
+    var completed_item = $(this).parent();
+    completed_item.find(".move-right").show();
+    
+    var prev_column = getPrevColumn(completed_item);
+    
+    if (isFirstColumn(prev_column)) {
+        completed_item.find(".move-left").hide();
+    }
+    
+    getPrevColumn(completed_item).find(".column-entries").prepend(completed_item);
 });
+
+function getNextColumn(element) {
+    return getColumn(element).next();
+}
+
+function getPrevColumn(element) {
+    return getColumn(element).prev();
+}
+
+function getColumn (element) {
+    while (element.attr("class") != "column") {
+        element = element.parent();
+    }
+    return element;
+}
+
+function isLastColumn (column) {
+    return column.is(":last-child");
+}
+
+function isFirstColumn (column) {
+    return column.is(":first-child");
+}

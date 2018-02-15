335c03fc8d09b9de21b181a2acf1be7cbeedcb0b
Conner <conner.hunihan@gmail.com>
2018-02-15 15:48:16 -0800

    working code, to do: update material icon

--- _js/main.js
+++ _js/main.js
@@ -4,7 +4,7 @@ $(document).ready(
         var user_input = $('#todo-item-input').val();
         // alert(user_input);
 
-        $('#list_todo').prepend("<li> <button> Move me! </button>" + user_input + "</li>");
+        $('#list_todo').prepend("<li> <button> MARK AS COMPLETE </button>" + user_input + "</li>");
     })
 );
 

2ab153784fc4e7e96c6e2e966a98dae34713f0fa
michellecarney <michelle.r.carney@gmail.com>
2018-02-15 12:53:59 -0800

    added color changing button and new javascript to change lists

--- _js/main.js
+++ _js/main.js
@@ -4,7 +4,7 @@ $(document).ready(
         var user_input = $('#todo-item-input').val();
         // alert(user_input);
 
-        $('#list_todo').prepend("<li> <button> Move me! </button>" + user_input + "</li>");
+        $('#list_todo').prepend("<li> <button class='libtn'> Move me! </button>" + user_input + "</li>");
     })
 );
 
@@ -20,4 +20,15 @@ $("#list_todo").on('click', "button", function() {
 
 $("#list_doing").on('click', "button", function() {
         // move back from list_doing container to list_todo container
+        $(this).html("Add To Doing");
+
+        var completedItem = $(this).parent()
+        $("#list_todo").prepend(completedItem);
+});
+
+var colors = ['red', 'green', 'blue', 'orange', 'yellow'];
+
+$('#new-item').click(function() {
+    //Now just reference this button and change CSS
+    $(this).css('background-color', colors[Math.floor(Math.random() * colors.length)]);
 });

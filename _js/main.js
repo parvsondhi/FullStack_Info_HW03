c60fdd9318b703ec8ee7ad918aac602c32f617fa
Bonney <bonney@berkeley.edu>
2018-02-15 15:15:12 -0800

    finished hw3

--- _js/main.js
+++ _js/main.js
@@ -4,7 +4,7 @@ $(document).ready(
         var user_input = $('#todo-item-input').val();
         // alert(user_input);
 
-        $('#list_todo').prepend("<li> <button> Move me! </button>" + user_input + "</li>");
+        $('#list_todo').prepend("<li> <button> Start </button>" + user_input + "</li>");
     })
 );
 
@@ -12,12 +12,18 @@ $("#list_todo").on('click', "button", function() {
         // move from list_todo container to list_doing container
         // console.log($(this).parent());
 
-        $(this).html("Add To To-Do");
+        $(this).html("Later");
 
-        var completedItem = $(this).parent()
+        var completedItem = $(this).parent();
         $("#list_doing").prepend(completedItem);
 });
 
 $("#list_doing").on('click', "button", function() {
         // move back from list_doing container to list_todo container
+
+        $(this).html("Start");
+
+        var completedItem = $(this).parent()
+        $("#list_todo").prepend(completedItem);
+
 });

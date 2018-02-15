29527ed43f48a5458b1da6dc27647309f04d8ac4
daphnejong <daphne.jong@berkeley.edu>
2018-02-15 15:25:49 -0800

    extra credit

--- _js/main.js
+++ _js/main.js
@@ -4,7 +4,8 @@ $(document).ready(
         var user_input = $('#todo-item-input').val();
         // alert(user_input);
 
-        $('#list_todo').prepend("<li> <button id='listbutton'> Move me! </button>" + user_input + "</li>");
+        $('#list_todo').prepend("<li> <button id='listbutton'> Move me! </button> <button id='done'> Add to Done </button>" + user_input + "</li>");
+        $('#list_todo #done').hide();
     })
 );
 
@@ -15,13 +16,35 @@ $("#list_todo").on('click', "button", function() {
 
         var completedItem = $(this).parent()
         $("#list_doing").prepend(completedItem);
+        $('#list_doing #done').show().css("background-color", "#279ec1");
 });
 
 
 $("#list_doing").on('click', "button", function() {
         // move back from list_doing container to list_todo container
-       $(this).html("Move me!");
+    if (this.id === 'listbutton') {
+        $(this).html("Move me!");
 
         var completedItem = $(this).parent()
         $("#list_todo").prepend(completedItem);
+    } else {
+        $(this).hide();
+
+        var completedItem = $(this).parent();
+        $("#list_done").prepend(completedItem);
+
+        $(this).parent().children('#listbutton').html("Add to Doing");
+    }
+
+    $('#list_todo #done').hide();
+});
+
+$("#list_done").on('click', "button", function() {
+        // move from list_todo container to list_doing container
+        // console.log($(this).parent());
+        $(this).html("Add to To-Do");
+
+        var completedItem = $(this).parent()
+        $("#list_doing").prepend(completedItem);
+        $(this).parent().children('#done').show().css("background-color", "#279ec1");
 });

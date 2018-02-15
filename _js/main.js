757ee32915a4280bf10114e33e78dec61cc7886e
michellecarney <michelle.r.carney@gmail.com>
2018-02-15 14:16:07 -0800

    added more styling and javascript

--- _js/main.js
+++ _js/main.js
@@ -4,7 +4,7 @@ $(document).ready(
         var user_input = $('#todo-item-input').val();
         // alert(user_input);
 
-        $('#list_todo').prepend("<li> <button class='libtn'> Move me! </button>" + user_input + "</li>");
+        $('#list_todo').prepend("<li class='item'> <button class='libtn'> 🔘 </button>" + user_input + "</li>");
     })
 );
 
@@ -12,7 +12,7 @@ $("#list_todo").on('click', "button", function() {
         // move from list_todo container to list_doing container
         // console.log($(this).parent());
 
-        $(this).html("Add To To-Do");
+        $(this).html("✅");
 
         var completedItem = $(this).parent()
         $("#list_doing").prepend(completedItem);
@@ -20,15 +20,15 @@ $("#list_todo").on('click', "button", function() {
 
 $("#list_doing").on('click', "button", function() {
         // move back from list_doing container to list_todo container
-        $(this).html("Add To Doing");
+        $(this).html("🔘");
 
         var completedItem = $(this).parent()
         $("#list_todo").prepend(completedItem);
 });
 
-var colors = ['red', 'green', 'blue', 'orange', 'yellow'];
+var colors = ["#FFCECE", "#FFC8F2", "#FFC8E3", "#FFCAF9", "#F5CAFF", "#F0CBFE", "#DDCEFF", "#ACF3FD", "#B5FFFC", "#A5FEE3", "#B5FFC8"];
 
 $('#new-item').click(function() {
-    //Now just reference this button and change CSS
+    //this sjust changes the "add item" button color every time you click
     $(this).css('background-color', colors[Math.floor(Math.random() * colors.length)]);
 });

94caff7a51b585fe51ba0c765dc2c6df69bdf53f
Nic-Mon <n_mon@berkeley.edu>
2018-02-15 16:37:23 -0800

    hw 3

--- _js/main.js
+++ _js/main.js
@@ -2,9 +2,11 @@ $(document).ready(
     $("#new-item").on('click', function() {
         // once the document loads, create new item with this function
         var user_input = $('#todo-item-input').val();
+
+        $('#todo-item-input').val('')
         // alert(user_input);
 
-        $('#list_todo').prepend("<li> <button> Move me! </button>" + user_input + "</li>");
+        $('#list_todo').prepend("<li> <button> ➡️ </button>" + " " + user_input + "</li>");
     })
 );
 
@@ -12,7 +14,7 @@ $("#list_todo").on('click', "button", function() {
         // move from list_todo container to list_doing container
         // console.log($(this).parent());
 
-        $(this).html("Add To To-Do");
+        $(this).html(" ✅ ");
 
         var completedItem = $(this).parent()
         $("#list_doing").prepend(completedItem);
@@ -20,4 +22,10 @@ $("#list_todo").on('click', "button", function() {
 
 $("#list_doing").on('click', "button", function() {
         // move back from list_doing container to list_todo container
+        $(this).html(" ➡️ ");
+
+        var todoItem = $(this).parent()
+        $("#list_todo").prepend(todoItem);
+
 });
+

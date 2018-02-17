193e6253d8d55d3de9d2343626a36728acdb4c00
root <root@DESKTOP-FC75PKR.localdomain>
2018-02-17 13:35:33 -0800

    updated code

--- _js/main.js
+++ _js/main.js
@@ -2,47 +2,48 @@ $(document).ready(
     $("#new-item").on('click', function() {
         // once the document loads, create new item with this function
         var user_input = $('#todo-item-input').val();
-        // create two buttons for each task to move it across the lists
-        $('#list_todo').prepend("<li> <button id='to_left'> << </button>" + user_input + " <button id='to_right'> >> </button> </li>");
-        
-        
-//        var w = $('#list_todo').width();
-//        $("button").siblings().css({"color": "red", "width": "w"});
-//        console.log(w);
-        
-        // hide the left pointing arrow from to-do list
-        $('#to_left').css('display', 'none');
+        // add this item to to-do list, apply styling and add button
+        $('#list_todo').prepend("<li class='task'> " + user_input + "<button class='to_right'> >> </button></li>");
+        $(".task").css({"color": "darkblue", "text-aligne": "justify"});
     })
 );
 
-$("#list_todo").on('click', 'button', function() {
-        // move from list_todo container to list_doing container
-    var takenItem = $(this).parent()
-    $("#list_doing").prepend(takenItem);
-    $('#to_left').css('display', 'inline'); //show left pointing arrow
+$("#list_todo").on('click', ".to_right", function() {
+        // move from list_todo container to list_doing container, 
+        //apply new style and add another button
+        var takenItem = $(this).parent();
+        $("#list_doing").prepend(takenItem);
+        $("button.to_left", $("#list_doing")).remove();
+        $("li.task", $("#list_doing")).prepend("<button class='to_left'> << </button>");
+        $("li.task", $("#list_doing")).css({"color": "blue"});
 });
 
-$("#list_doing").on('click', '#to_right', function() {
-        //move from list doing to list done
-    
-    //console.log($(this).parent());
-    var completedItem = $(this).parent()
-    $("#list_done").prepend(completedItem);
-    $('#to_right').css('display', 'none');
-});
-
-$("#list_doing").on('click', '#to_left', function() {
+$("#list_doing").on('click', ".to_left", function() {
         // move back from list_doing container to list_todo container
-    var nottakenItem = $(this).parent()
-    $("#list_todo").prepend(nottakenItem);
-    $('#to_left').css('display', 'none');
+        // return "to-do" styling and the button
+        var droppedItem = $(this).parent()    
+        $("#list_todo").prepend(droppedItem);
+        $("button.to_left", $("#list_todo")).remove();
+        $("li.task", $("#list_todo")).css({"color": "darkblue"});
 });
 
-
-$("#list_done").on('click', 'button', function() {
-   // move back from list_done container to list_doing container
-    var uncompletedItem = $(this).parent()
-    $("#list_doing").prepend(uncompletedItem);
-    $('#to_right').css('display', 'inline');
+$("#list_doing").on('click', ".to_right", function() {
+        // move forward from list_doing container to list_done container
+        // apply new styling and adjust buttons
+        var completedItem = $(this).parent()    
+        $("#list_done").prepend(completedItem);
+        $("button.to_right", $("#list_done")).remove();
+        $("li.task", $("#list_done")).css({"color": "lightgrey"});
 });
+
+$("#list_done").on('click', ".to_left", function() {
+        // move back from list_done container to list_doing container
+        // return the "doing" styling and adjust buttons
+        var incompleteItem = $(this).parent()    
+        $("#list_doing").prepend(incompleteItem);
+        $("button.to_right", $("#list_doing")).remove();
+        $("li.task", $("#list_doing")).append("<button class='to_right'> >> </button>");
+        $("li.task", $("#list_doing")).css({"color": "blue"});
+});
+
 
